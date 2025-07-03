752a7c4552a1a974e6f817931b3c13805375e1d1
Andrew Poland <andrew@chipp.ai>
2025-07-03 17:57:30 -0400

    atom feed for job board

--- pb/hooks/atom.pb.js
+++ pb/hooks/atom.pb.js
@@ -1,9 +1,37 @@
 /// <reference path="../data/types.d.ts" />
 
-routerAdd("get", "/jobs.atom", (e) => {
-  const xml = $template.loadFiles(
-    `${__hooks}/views/atom_layout.html`, `${__hooks}/views/atom.html`,
-  ).render();
+routerAdd("get", "/jobs_test.atom", (e) => {
+  const template = $template.loadFiles(
+    `${__hooks}/views/atom.html`,
+  );
 
-  return e.blob(200, 'application/atom+xml', xml)
-})
+
+  let records = $app.findRecordsByFilter(
+    "jobs",                                    // collection
+    "", // filter
+    "-created",                                   // sort
+    20,                                            // limit
+    0,                                             // offset
+    { },                        // optional filter params
+  )
+
+  const entries = records.map((r) => ({
+    title:   r.getString("title"),
+    link:    `https://indyhackers.org/posts/${r.id}`,
+    id:      `tag:indyhackers.org,2025:post/${r.id}`,
+    updated: r.getDateTime("created").toString(),
+    summary: r.getString("summary") || "",
+  }));
+
+  const xml = template.render({
+    title:   "IndyHackers Job Board Atom Feed",
+    updated: new Date().toISOString(),
+    feedID:  "https://indyhackers.org/",
+    entries
+  });
+
+  // 6) respond as Atom XML
+  return e
+    .blob(200, "application/atom+xml; charset=utf-8", xml)
+
+});
